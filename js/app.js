3f878c1aa3e9f6bb40b9c07a9463b076ee8c347e
Josh Painter <jpainter4@gmail.com>
2015-01-16 14:06:23 -0800

    list meetup groups

--- js/app.js
+++ js/app.js
@@ -1,28 +1,39 @@
+var city = '';
+var state = '';
+var zip = '';
+var country = '';
+var lat = '';
+var lng = '';
+
 $(document).ready( function() {
 	initialize();
 	$('#search-form').submit( function(event){
 		console.log('search-form submit');
 		$(this).find("input[name='tags']").val();
-		getMeetups($('#group').val(), $('#distance').val(), $('#location').val());
+		getMeetups($('#group').val(), $('#distance').val());
 	});
 });
 
-function getMeetups (group, distance, location) {
-	console.log('getMeetups(' + group + ', ' + distance + ', ' + location + ')');
+function getMeetups (group, distance) {
+	console.log('getMeetups(' + group + ', ' + distance);
 
 	// the parameters we need to pass in our request to StackOverflow's API
-	var request = {tagged: tags,
-								site: 'stackoverflow',
-								order: 'desc',
-								sort: 'creation'};
-	
+	var request = {key: '24a3d6c32673b27346e265224741b1b',
+								topic: group,
+								lat: lat,
+								lon: lng};
+	console.log(request);
 	var result = $.ajax({
-		url: "http://api.stackexchange.com/2.2/questions/unanswered",
+		url: "https://api.meetup.com/2/groups",
 		data: request,
 		dataType: "jsonp",
 		type: "GET",
 		})
 	.done(function(result){
+		console.log(result);
+		console.log(result.results);
+		listMeetups(result.results);
+		/*
 		var searchResults = showSearchResults(request.tagged, result.items.length);
 
 		$('.search-results').html(searchResults);
@@ -31,19 +42,30 @@ function getMeetups (group, distance, location) {
 			var question = showQuestion(item);
 			$('.results').append(question);
 		});
+	*/
 	})
 	.fail(function(jqXHR, error, errorThrown){
+		/*
 		var errorElem = showError(error);
 		$('.search-results').append(errorElem);
+		*/
 	});
+}
+
+function listMeetups(groups) {
+
+ 	var items = [];
+
+	$.each(groups, function(i, item) {
+		items.push('<li><a href="' + item.link + '">' + item.name + '</a></li>');
+	});
+	$('#group-list').append( items.join('') );
+
 }
 
 //Autocompletion of place input
 
 var autocomplete;
-var city = '';
-var state = '';
-var zip = '';
 
 function initialize() {
   // Create the autocomplete object, restricting the search
@@ -54,35 +76,11 @@ function initialize() {
   // When the user selects an address from the dropdown,
   // populate the address fields in the form.
   google.maps.event.addListener(autocomplete, 'place_changed', function() {
-		getAddress();
+		var place = autocomplete.getPlace();
+		lat = place.geometry.location.lat();
+  		lng = place.geometry.location.lng();
+  		console.log('latitude: ' + lat + ' longitude: ' + lng);
   });
-}
-
-// [START region_fillform]
-function getAddress() {
-	 // Get the place details from the autocomplete object.
-	var place = autocomplete.getPlace();
-	// Get each component of the address from the place details
-	for (var i = 0; i < place.address_components.length; i++) {
-		var t = place.address_components[i].types;
-		if(compIsType(t, 'administrative_area_level_1'))
-			state = place.address_components[i].long_name; //store the state
-		else if(compIsType(t, 'locality'))
-			city = place.address_components[i].long_name; //store the city
-		else if(compIsType(t, 'postal_code'))
-			zip = place.address_components[i].long_name;
-	}
-	console.log(city + ' ' + state + ' ' + zip);
-
-}
-
-function compIsType(t, s) { 
-	for(z = 0; z < t.length; ++z) {
-		if(t[z] == s) {
-			return true;
-		}
-	}
-	return false;
 }
 
 // [START region_geolocation]
