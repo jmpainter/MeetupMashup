eb81cfa4103522ae4613d98ad30fa6e452f35575
Josh Painter <jpainter4@gmail.com>
2015-01-16 15:40:31 -0800

    fix list clear issue

--- js/app.js
+++ js/app.js
@@ -1,7 +1,3 @@
-var city = '';
-var state = '';
-var zip = '';
-var country = '';
 var lat = '';
 var lng = '';
 var map = null;
@@ -9,21 +5,18 @@ var map = null;
 $(document).ready( function() {
 	initialize();
 	$('#search-form').submit( function(event){
-		console.log('search-form submit');
 		$(this).find("input[name='tags']").val();
 		getMeetups($('#group').val(), $('#distance').val());
 	});
 });
 
 function getMeetups (group, distance) {
-	console.log('getMeetups(' + group + ', ' + distance);
 
 	// the parameters we need to pass in our request to StackOverflow's API
 	var request = {key: '24a3d6c32673b27346e265224741b1b',
 								topic: group,
 								lat: lat,
 								lon: lng};
-	console.log(request);
 	var result = $.ajax({
 		url: "https://api.meetup.com/2/groups",
 		data: request,
@@ -50,12 +43,12 @@ function showMeetups(groups) {
 	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 	$('#map-canvas').css('border', '1px solid #a3a3a3');
 
+	$('#group-list').empty();
 	//this array will store the html to be appended to the results list
- 	var items = [];
+	var items = [];
 
 	$.each(groups, function(i, item) {
 		items.push('<li><a href="' + item.link + '">' + item.name + '</a></li>');
-		console.log('name: ' + item.name + ' lat: ' + item.lat + ' lon: ' + item.lon);
 		var marker = new google.maps.Marker({
 			position: { lat: item.lat, lng: item.lon},
 			map: map,
@@ -80,29 +73,26 @@ function initialize() {
   autocomplete = new google.maps.places.Autocomplete(
 		/** @type {HTMLInputElement} */(document.getElementById('location')),
 		{ types: ['geocode'] });
+
+	// Bias the autocomplete object to the user's geographical location,
+	// as supplied by the browser's 'navigator.geolocation' object.
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+			var geolocation = new google.maps.LatLng(
+				 	position.coords.latitude, position.coords.longitude);
+			var circle = new google.maps.Circle({
+			  	center: geolocation,
+			  	radius: position.coords.accuracy
+			});
+			autocomplete.setBounds(circle.getBounds());
+		});
+	}
   // When the user selects an address from the dropdown,
-  // populate the address fields in the form.
+  // set the global latitude and longitude
   google.maps.event.addListener(autocomplete, 'place_changed', function() {
 		var place = autocomplete.getPlace();
 		lat = place.geometry.location.lat();
-  		lng = place.geometry.location.lng();
-  		console.log('latitude: ' + lat + ' longitude: ' + lng);
+		lng = place.geometry.location.lng();
+		console.log('setting lat: ' + lat + ' lon: ' + lng);
   });
-}
-
-// [START region_geolocation]
-// Bias the autocomplete object to the user's geographical location,
-// as supplied by the browser's 'navigator.geolocation' object.
-function geolocate() {
-  if (navigator.geolocation) {
-	 navigator.geolocation.getCurrentPosition(function(position) {
-		var geolocation = new google.maps.LatLng(
-			 position.coords.latitude, position.coords.longitude);
-		var circle = new google.maps.Circle({
-		  center: geolocation,
-		  radius: position.coords.accuracy
-		});
-		autocomplete.setBounds(circle.getBounds());
-	 });
-  }
 }
